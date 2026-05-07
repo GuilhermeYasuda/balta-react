f6bd4c1bc32fcbc300e29c25e087a9e3f55b7fea
Guilherme Harunobu Yasuda <haru_yasuda@hotmail.com>
2026-05-07 13:22:06 -0300

    feat: add todo functionality with context and form validation
    - Added AddTodo component for creating new todos with validation using react-hook-form and yup.
    - Implemented TodoContext for managing todo state across the application.
    - Created TodoList and TodoListItem components for displaying and managing todos.
    - Updated App component to include routing for creating and viewing todos.
    - Removed unused App.css and related files, and cleaned up index.tsx.
    - Added necessary dependencies for form handling and validation.
    - Updated index.html to include UIkit for styling.

---
+++ src/components/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TodoList from "./TodoList";
+
+const Home = () => {
+    return (
+        <div>
+            <h4>Minha lista de tarefas</h4>
+            <TodoList></TodoList>
+        </div>
+    );
+}
+
+export default Home;
